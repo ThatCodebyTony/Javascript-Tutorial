284eb4c4f3b0e63ba97cdc505201a00afc6cacdc
Tony Wu <twu1415@gmail.com>
2024-10-31 13:13:29 -0700

    Trying out textContent

--- index.js
+++ index.js
@@ -2,33 +2,44 @@
 
 // change the count-el in the HTML to reflect the new count
 
+let saveEl = document.getElementById("save-el")
 let countEL = document.getElementById("count-el")
 
 
 let count = 0
 
 function increment() {
-    count = count + 1
+    count += 1
     countEL.innerText = count
 }
 
 function save(){
-    console.log(count)
+    let countStr = count + " - ";
+    saveEl.textContent += countStr;
+    console.log(count);
 }
 // ----------------------------------------------
-let username = "per"
-let message = "You have tree new notifications"
-console.log(message + ", " + username + "!")
+// let username = "per"
+// let message = "You have tree new notifications"
+// console.log(message + ", " + username + "!")
 
-let newMessage = message + ", " + username + "!"
+// let newMessage = message + ", " + username + "!"
 
-let name = "Tony"
-let greeting = "Hi, my name is"
+// let name = "Tony"
+// let greeting = "Hi, my name is"
 
-let myGreating = greeting + " " + name
+// let myGreating = greeting + " " + name
 // ----------------------------------------------
 console.log(4 + 5) //9
 console.log("2" + "4") //24
 console.log("5" + 1) //51
 console.log(100 + "100") //100100
 // ----------------------------------------------
+
+
+let welcomeEl = document.getElementById("welcome-el")
+let name = "Per Dude"
+let greeting = "Welcome back, "
+let myGreeting = greeting + name
+welcomeEl.innerText = myGreeting
+welcomeEl.innerText += " " + "👋" + "Homie"
