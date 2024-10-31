3d7d282fbe0313d258770b3d85a65181662beaf9
Tony Wu <twu1415@gmail.com>
2024-10-31 12:35:57 -0700

    added save button

--- index.js
+++ index.js
@@ -3,12 +3,15 @@
 // change the count-el in the HTML to reflect the new count
 
 let countEL = document.getElementById("count-el")
-console.log(countEL)
+
 
 let count = 0
 
 function increment() {
     count = count + 1
     countEL.innerText = count
+}
+
+function save(){
     console.log(count)
 }
